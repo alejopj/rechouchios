cd7d8f0da413d9f917ee1a877aac35631506bb26
Alejo Pacín Jul <alejopj@gmail.com>
2015-11-11 22:29:03 +0100

    Fixed several issues with the Twitter service. Working as expected.

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,18 +12,11 @@ router.post('/login', auth.login);
 /*
  * Routes that can be accessed only by autheticated users
  */
-router.get('/api/v1/tweets' +
-           '?query=:query' +
-           '&geocode=:geocode' +
-           '&count=:count' +
-           '&since=:since' +
-           '&since_id=:since_id' +
-           '&max_id=:max_id',
-           twitter.search);
+router.get('/api/v1/tweets', twitter.search);
  
 /*
  * Routes that can be accessed only by authenticated & authorized users
  */
-router.get('/api/v1/admin/tweets', twitter.search);
+// router.get('/api/v1/admin/tweets', twitter.search);
  
 module.exports = router;
--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -12,12 +12,12 @@ var twitter = {
 
     search: function(req, res) {
         
-        var q = req.params.query;
-        var geocode = req.params.geocode;
-        var count = req.params.count;
-        var since = req.params.since;
-        var since_id = req.params.since_id;
-        var max_id = req.params.max_id;
+        var q = req.query.query || '';
+        var geocode = req.query.geocode;
+        var count = req.query.count;
+        var since = req.query.since;
+        var since_id = req.query.since_id;
+        var max_id = req.query.max_id;
         
         q += geocode ? ' geocode:' + geocode : '';
         q += ' since:' + (since || getToday());
@@ -28,7 +28,8 @@ var twitter = {
         
         _app.get('search/tweets', {'q': q, 'count': count}, function(err, data, response) {
             
-            console.log(data);
+            res.status(200);
+            res.json(data);
             
         });
         
